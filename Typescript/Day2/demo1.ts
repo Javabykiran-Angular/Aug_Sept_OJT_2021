3586b87f253770a80fef4d6a16963e3d6fb443c5
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-10-21 12:33:15 +0530

    Day2

---
+++ Typescript/Day2/demo1.ts
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+//var a1;
+/*var a2,b2;
+var a_2,b$a;*/
+
+// datatype => number(int,long,double,float,short),
+// string (string and charcter ""/''),boolean(true/false),
+// any(number,string,boolean) , 
+// void (netural datatype=> It does not point any thing)
+// misce. null & undefined => it acts as data type as well as a value
+
+var a1:number=10;   //Forward declration & defination
+console.log('Value of a1 is '+a1);
+console.log(`value of a1= ${a1} `)
+
+
