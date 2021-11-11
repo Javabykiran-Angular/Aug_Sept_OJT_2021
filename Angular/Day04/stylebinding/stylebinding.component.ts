4fbe5ce206c799710822a8c854abb966bfab48f9
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-11-11 12:39:51 +0530

    Day04

---
+++ Angular/Day04/stylebinding/stylebinding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stylebinding',
+  templateUrl: './stylebinding.component.html',
+  styleUrls: ['./stylebinding.component.css']
+})
+export class StylebindingComponent implements OnInit {
+
+  mycolor:string='red';
+  rating:number=21;
+  jsonObj={
+    color:'blue',
+    fontStyle:"italic",
+    "font-size":'32px'
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
