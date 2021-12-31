ba5293d929e981407f66541f88f07fc74011533f
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-12-31 15:13:30 +0530

    Day32

---
+++ Angular/Day32/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+// import { NgxCaptchaModule } from 'ngx-captcha';
+
+import { ParticlesModule } from 'angular-particle'; 
+
+import {  HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HeaderComponent } from './header/header.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { EmployeDetailsComponent } from './employe-details/employe-details.component';
+import { TdfComponent } from './tdf/tdf.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component'; 
+import { NgxPaginationModule } from 'ngx-pagination';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NotfoundComponent,
+    HeaderComponent,
+    AddemployeeComponent,
+    EmployeDetailsComponent,
+    TdfComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+   // NgxCaptchaModule,
+    ParticlesModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    NgxPaginationModule
+   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/Day32/home/home.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from '../http.service';
+import { BsModalService,BsModalRef } from 'ngx-bootstrap/modal';
+import { Employee } from '../models/employee';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  EmpData:any[]=[];
+  isRadioClick:boolean=false;
+
+  modalRef: BsModalRef;  
+
+  config = {
+    animated: true,
+    
+    ignoreBackdropClick: true,
+    class: "alert alert-danger"
+  };
+  
+  empobj=<Employee>{};
+
+  p:number=1;
+
+  constructor(private service:HttpService,
+              private router:Router,
+              private modalservice:BsModalService) { }
+
+  ngOnInit() {
+    this.GetAllEmoloyee();
+  }
+
+  GetAllEmoloyee(){
+      this.service.GetAllEmployee()
+      .subscribe((response)=>{
+        console.log(response);
+        this.EmpData=(<any>response);
+      })
+  }
+
+  onAddEmployee(){
+      this.router.navigate(["/addEmployee"]);
+  }
+  onRadioClick(item){
+    this.isRadioClick=true;
+    // console.log(item);
+    this.empobj=item;
+  }
+
+  radioCheck(){
+    if(this.isRadioClick==true){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  onEdit(popup: TemplateRef<any>){
+      if(this.radioCheck()){
+        //logic
+
+        this.modalRef= this.modalservice.show(popup,this.config)
+
+
+      }else{
+        alert("Please select a record to upadte...");
+      }
+
+
+  }
+
+  onDelete(){
+
+    if(this.radioCheck()){
+      //logic
+    }else{
+      alert("Please select a record to Delete...");
+    }
+
+  }
+
+
+
+}
